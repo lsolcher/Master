5f3156d0c1eeeaca54ed2cb50b5006c5b6d86749
Lucas <s0539248@htw-berlin.de>
2018-06-04 23:57:25 +0200

    analyze distance and get min distance

--- Analyzer/app.py
+++ Analyzer/app.py
@@ -7,21 +7,42 @@ Created on Mon May 28 19:44:00 2018
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import euclidean_distances
+import os
+from os.path import isfile, join
 import numpy as np
 import scipy
+import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
 
-SPONDIRPATH = 'C:/Programmierung/Masterarbeit/Scraper/SPON/articles/'
+DIRPATH = 'C:/Programmierung/Masterarbeit/Scraper/data/articles/'
+RESULTPATH = 'C:/Programmierung/Masterarbeit/Analyzer/results/'
+SPONPATH = os.path.join(DIRPATH, 'SPON')
+ZEITPATH = os.path.join(DIRPATH, 'ZEIT')
 
-files = [open(SPONDIRPATH + 'SPON_0.txt', 'r'), open(SPONDIRPATH + 'SPON_1.txt', 'r'), open(SPONDIRPATH + 'testfile.txt', 'r')]
+sponFiles = []
+for file in os.listdir(SPONPATH):
+    if file.endswith(".txt"):
+        sponFiles.append(open(os.path.join(SPONPATH, file), encoding="utf8", errors='ignore').read())
+zeitFiles = []
+for file in os.listdir(ZEITPATH):
+    if file.endswith(".txt"):
+        zeitFiles.append(open(os.path.join(ZEITPATH, file), encoding="utf8", errors='ignore').read())
 
-vectorizer = CountVectorizer(input='file')
+
+#files = [open(SPONDIRPATH + 'SPON_0.txt', 'r'), open(SPONDIRPATH + 'SPON_1.txt', 'r'), open(SPONDIRPATH + 'testfile.txt', 'r')]
+files = sponFiles + zeitFiles
+print(len(sponFiles))
+print(len(zeitFiles))
+print(len(files))
+
+
+
+vectorizer = CountVectorizer(input='files')
 dtm = vectorizer.fit_transform(files)
 vocab = vectorizer.get_feature_names()
 scipy.sparse.csr.csr_matrix
 dtm = dtm.toarray()
 vocab = np.array(vocab)
-house_idx = list(vocab).index('aber')
-dtm[0, vocab == 'house']
 n, _ = dtm.shape
 dist = np.zeros((n, n))
 for i in range(n):
@@ -29,8 +50,82 @@ for i in range(n):
         x, y = dtm[i, :], dtm[j, :]
         dist[i, j] = np.sqrt(np.sum((x - y)**2))
 dist = euclidean_distances(dtm)
+#resultfile = open(os.path.join(RESULTPATH, 'result.csv'), 'wb')
+#resultfile.write(dtm)
+print(dtm)
 np.round(dist, 1)
 print(dist[0, 2])
+min_dist = 1000
+for i in range(438):
+    for j in range(48):
+        print(i)
+        print(j)
+        thisDist = dist[i, 438 + j]
+        print(thisDist)
+        if min_dist > thisDist:
+            print('new min dist:')
+            print(min_dist)
+            min_dist = thisDist
+            sponA = i
+            zeitA = j
+print(sponA)
+print(zeitA)
+print()
+
+
+# Visualization
+"""
+mds = MDS(n_components=2, dissimilarity="precomputed", random_state=1)
+pos = mds.fit_transform(dist)
+xs, ys = pos[:, 0], pos[:, 1]
+names = [os.path.basename(fn).replace('.txt', '') for fn in files]
+print('show plot')
+for x, y, name in zip(xs, ys, names):
+    color = 'orange' if "Austen" in name else 'skyblue'
+    plt.scatter(x, y, c=color)
+    plt.text(x, y, name)
+plt.show()
+"""
+#scipy.spatial.distance.pdist(dist)
+#print(scipy.spatial.distance.pdist(dist))
+
+
+"""
+vectorizerSpon = CountVectorizer(input='sponFiles')
+dtmSpon = vectorizerSpon.fit_transform(sponFiles)
+vocab = vectorizerSpon.get_feature_names()
+scipy.sparse.csr.csr_matrix
+dtmSpon = dtmSpon.toarray()
+vocab = np.array(vocab)
+n, _ = dtmSpon.shape
+distSpon = np.zeros((n, n))
+for i in range(n):
+    for j in range(n):
+        x, y = dtmSpon[i, :], dtmSpon[j, :]
+        distSpon[i, j] = np.sqrt(np.sum((x - y)**2))
+np.round(distSpon, 1)
+
+        
+vectorizerZeit = CountVectorizer(input='zeitFiles')
+dtmZeit = vectorizerZeit.fit_transform(zeitFiles)
+vocab = vectorizerZeit.get_feature_names()
+scipy.sparse.csr.csr_matrix
+dtmZeit = dtmZeit.toarray()
+vocab = np.array(vocab)
+n, _ = dtmZeit.shape
+dist = np.zeros((n, n))
+for i in range(n):
+    for j in range(n):
+        x, y = dtmZeit[i, :], dtmZeit[j, :]
+        dist[i, j] = np.sqrt(np.sum((x - y)**2))
+distZeit = euclidean_distances(dtmZeit)
+
+
+#resultfile = open(os.path.join(RESULTPATH, 'result.csv'), 'wb')
+#resultfile.write(dtm)
+#print(dtm)
+print(dist[0, 2])
+"""
 
 """
 file = open(SPONDIRPATH + 'SPON_0.txt', 'r') 
